56f714a95b9a2b68bb3eb6aceccb5a1439f5d454
Campbell Reid Tait <campbellreidtait@gmail.com>
2014-08-20 11:36:41 +1200

    updating h files

---
+++ code/ASC/ADC.h
@@ -0,0 +1,20 @@
+//*****************************************************************************
+//!	ENCE463 Project
+//! Author: Campbell Reid-Tait
+//! Date: 20 August 2014
+//! file: "ADC.h"
+//!
+//! This module does everything to do with the adc
+//*****************************************************************************
+#ifndef ADC_H_
+#define ADC_H_
+
+/*called to read a certain ADC*/
+extern float readADC(int toreadADC );
+
+/*initilises a number of ADC inputs*/
+extern void initADC(int howmany);
+
+
+
+#endif /* ADC_H_ */
---
+++ code/ASC/ride.h
@@ -0,0 +1,16 @@
+//*****************************************************************************
+//!	ENCE463 Progect
+//! Author: Campbell Reid-Tait
+//! Date: 20 August 2014
+//! file: "ride.h"
+//!
+//! This modules changes the current ride infomation 
+//*****************************************************************************
+
+#ifndef RIDE_H_
+#define RIDE_H_
+
+extern void changeRide (int newRide);
+extern int returnCurrentRide(void);
+
+#endif /* RIDE_H_ */

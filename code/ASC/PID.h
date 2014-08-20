b5fda484a9cb67e55701a7a25cbd7b845fd75ae9
Campbell Reid Tait <campbellreidtait@gmail.com>
2014-08-20 15:49:24 +1200

    more h stuff

---
+++ code/ASC/PID.h
@@ -0,0 +1,16 @@
+//*****************************************************************************
+//!	ENCE463 Project
+//! Author: Campbell Reid-Tait
+//! Date: 20 August 2014
+//! file: "PID.h"
+//!
+//! This module does everything to do with the pid controller
+//*****************************************************************************
+#ifndef PID_H_
+#define PID_H_
+
+extern float calcPID (int info);
+
+
+
+#endif /* PID_H_ */
